18b4566e472e95b91ce85be438aaf3f19ef4613f
pqqanqing <15601870402@163.com>
2017-08-21 13:59:26 +0800

    Initial commit

---
+++ src/main/java/com/demo/config/demo/DemoConfigRepoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.config.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConfigRepoApplication.class, args);
+	}
+}
